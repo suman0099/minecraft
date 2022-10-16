b79a5a3ee929e2fa4ff52b4e6f04c07f02cfb083
Suman Mahato <suman.mahato@komprise.com>
2022-10-16 20:36:31 +0530

    Textures and images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 
 function App() {
   return (
     <>
-      <div>Outside Canvas</div>
       <Canvas>
-       </Canvas>
+        <Sky sunPosition={[100, 100, 20]} />
+      </Canvas>
     </>
   );
 }
